11b074ddc46e4183513f733d7b61f6ccdbd812cf
JohnZBurnett <tuninator@gmail.com>
2018-08-09 13:42:16 -0400

    implement initial app tests

--- server/client/src/App.test.js
+++ server/client/src/App.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() }); 
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = shallow(<App />)
+  })
 
   it('should render a <div />', () => {
-    let wrapper = shallow(< App />)
     expect(wrapper.find('div').length).toEqual(1); 
   });
+
+  it('should mount in the DOM', () => {
+    expect(mount(<App />).find('div').length).toEqual(1);
+  })
+
+  it('should render to static HTML', function() {
+    expect(render(< App />).text()).toEqual('My App'); 
+  })
 })
