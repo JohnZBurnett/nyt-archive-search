84cfe90c4a9bb6483665cd561ffa699c87d45ef8
JohnZBurnett <tuninator@gmail.com>
2018-08-09 16:11:09 -0400

    create actionTypes

---
+++ server/client/src/actions/actionTypes.js
@@ -0,0 +1,11 @@
+const GET_CURRENT_USER = "get_current_user";
+const GET_CURRENT_ARTICLE = "get_current_article";
+const UPDATE_CURRENT_ARTICLE = "update_current_article";
+const SAVE_ARTICLE = "save_article"; 
+
+module.exports = {
+    GET_CURRENT_ARTICLE,
+    GET_CURRENT_USER,
+    UPDATE_CURRENT_ARTICLE,
+    SAVE_ARTICLE
+}
---
+++ server/client/src/actions/index.js
@@ -0,0 +1,6 @@
+import { UPDATE_CURRENT_ARTICLE, GET_CURRENT_USER, GET_CURRENT_ARTICLE, SAVE_ARTICLE} from './actionTypes';
+
+{
+    type: GET_CURRENT_USER
+    username: "placeholder"
+};
