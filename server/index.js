598522aaa71f9b0fa90da3691717dc5b9fcb42b8
JohnZBurnett <tuninator@gmail.com>
2018-08-10 14:42:51 -0400

    redoing last commit in proper server folder

--- server/index.js
+++ server/index.js
@@ -2,15 +2,20 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const Article = require('./models/Article'); 
 const express = require('express');
+const PORT = process.env.PORT || 5000; 
 
 mongoose.connect(keys.MONGO_DEV_URI);
 
-async function getAllArticles() {
-    const allArticles = await Article.find({
-    }); 
-    console.log("LENGTH OF ALL ARTICLES: ", allArticles.length); 
-    console.log(allArticles[0].snippet);
-    mongoose.connection.close();
-}
+const app = express(); 
+require('./routes/apiRoutes')(app); 
 
-getAllArticles(); 
+app.listen(PORT); 
+// async function getAllArticles() {
+//     const allArticles = await Article.find({
+//     }); 
+//     console.log("LENGTH OF ALL ARTICLES: ", allArticles.length); 
+//     console.log(allArticles[0].snippet);
+//     mongoose.connection.close();
+// // }
+
+// getAllArticles(); 
