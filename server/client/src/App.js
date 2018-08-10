3f69677d0d56ecf905c94c5ee73d08b95ef17368
JohnZBurnett <tuninator@gmail.com>
2018-08-10 10:31:43 -0400

    install react-router-dom

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route} from 'react-router';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';  
 
 const App = () => {
