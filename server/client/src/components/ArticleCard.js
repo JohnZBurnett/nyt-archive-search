346b10842b478a8d2fa138e565db5f8479a73930
JohnZBurnett <tuninator@gmail.com>
2018-08-11 15:03:39 -0400

    add developmental styling to ArticleCard

--- server/client/src/components/ArticleCard.js
+++ server/client/src/components/ArticleCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArticleCard = (props) => {
     console.log("ARTICLE CARD PROPS: ", props); 
-    return(<div>
+    return(<div className="card">
         <h5>{props.article.headline.main}</h5>
         <p>{props.article.snippet}</p>
     </div>);
