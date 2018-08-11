7a03d02a982fc31c823e64af90025ec0272fcbd5
JohnZBurnett <tuninator@gmail.com>
2018-08-11 17:54:34 -0400

    add jwt & crypto to User schema; update schema; write setPassword method

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose'); 
 const { Schema } = mongoose;
+const crypto = require('crypto'); 
+const jwt = require('jsonwebtoken'); 
 
 const userSchema = new Schema({
-    name: String
+    name: String,
+    username: String,
+    hash: String,
+    salt: String
 }); 
+
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex'); 
+}
 
 const User = mongoose.model('users', userSchema); 
 const findAllUsers = function() {
