963087d276b31ca1a6fa09c425420c172562014b
JohnZBurnett <tuninator@gmail.com>
2018-08-11 15:27:25 -0400

    map state to ArticleDetail props

--- server/client/src/components/ArticleDetail.js
+++ server/client/src/components/ArticleDetail.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'; 
 
-const ArticleDetail = (props) => {
+
+const mapStateToProps = (state) => {
+    return(
+        {
+            article: state.currentArticle
+        }
+    )
+}
+
+const ArticleDetail = ({ article }) => {
+    console.log("ARTICLE DETAIL ARTICLE: ", article); 
     return(
         <div>I am an ArticleDetail placeholder.</div>
     );
 }
 
-export default ArticleDetail;
+export default connect(mapStateToProps)(ArticleDetail);
