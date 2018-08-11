15b388331451d5e95c867bd6b87ac2c5490aa3ef
JohnZBurnett <tuninator@gmail.com>
2018-08-11 15:01:44 -0400

    render ArticleCard w/ headline and snippet

--- server/client/src/components/ArticleCard.js
+++ server/client/src/components/ArticleCard.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ArticleCard = (props) => {
     console.log("ARTICLE CARD PROPS: ", props); 
-    return(<div>I am an article card!</div>);
+    return(<div>
+        <h5>{props.article.headline.main}</h5>
+        <p>{props.article.snippet}</p>
+    </div>);
 }
 
 export default ArticleCard; 
