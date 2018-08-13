7a391810f6fbfb30a9b0e3639fdcad0246a9b574
JohnZBurnett <tuninator@gmail.com>
2018-08-13 14:28:49 -0400

    added frontend test request to http://localhost:5000/api/current_article

--- server/pdf-url-seed-test.js
+++ server/pdf-url-seed-test.js
@@ -1,10 +1,9 @@
-  axios.get("https://timesmachine.nytimes.com/svc/tmach/v1/refer?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true")
-    .then( resp => {
-      pdf_url = resp.request.res.responseUrl.split(".html")[0]+".pdf";
-      console.log("PDF URL: ", pdf_url)
-    } ); 
+//   axios.get("https://timesmachine.nytimes.com/svc/tmach/v1/refer?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true")
+//     .then( resp => {
+//       pdf_url = resp.request.res.responseUrl.split(".html")[0]+".pdf";
+//       console.log("PDF URL: ", pdf_url)
+//     } ); 
 
-    "https://query.nytimes.com/gst/abstract.html?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true"
-    "?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true"
+//     "https://query.nytimes.com/gst/abstract.html?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true"
 
-    "https://timesmachine.nytimes.com/svc/tmach/v1/refer" + web_url.split(".html")[1]
+//     "https://timesmachine.nytimes.com/svc/tmach/v1/refer" + web_url.split(".html")[1]
