b7b42bfc09b5ec9770237d1f79fc54e5a8fc6b15
JohnZBurnett <tuninator@gmail.com>
2018-08-13 14:19:37 -0400

    run pdf scrape test successfully

--- server/pdf-url-seed-test.js
+++ server/pdf-url-seed-test.js
@@ -1,6 +1,7 @@
-  axios.get("https://query.nytimes.com/gst/abstract.html?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE")
+const axios = require('axios'); 
+  
+  axios.get("https://timesmachine.nytimes.com/svc/tmach/v1/refer?res=9A01E2DC143CEE3BBC4950DFB5668388659EDE&pdf=true")
     .then( resp => {
       pdf_url = resp.request.res.responseUrl.split(".html")[0]+".pdf";
       console.log("PDF URL: ", pdf_url)
     } ); 
-
