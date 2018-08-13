727a72af8ebf4c68991976dc1bf8c2bb0bbaae73
JohnZBurnett <tuninator@gmail.com>
2018-08-13 09:21:28 -0400

    add login form & control form

--- server/client/src/components/Login.js
+++ server/client/src/components/Login.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
+class Login extends Component {
+
+    constructor(props) {
+
+        super(); 
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }; 
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault(); 
+    }
+
+   render() {
     return(
-        <div>This is a Login placeholder.</div>
+        <div>
+            <form action="/api/login" method="post" onSubmit={this.handleFormSubmit}>
+    <div>
+        <label>Username:</label>
+        <input type="text" name="username" onChange={this.handleUsernameChange}/>
+    </div>
+    <div>
+        <label>Password:</label>
+        <input type="password" name="password" onChange={this.handlePasswordChange}/>
+    </div>
+    <div>
+        <input type="submit" value="Log In"/>
+    </div>
+</form>
+        </div>
     )
+   } 
 }
 
 export default Login; 
