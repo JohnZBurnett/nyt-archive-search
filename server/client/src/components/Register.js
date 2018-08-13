217c81c51e2ef0397a27d532add7bda6d4b0b69a
JohnZBurnett <tuninator@gmail.com>
2018-08-13 13:52:34 -0400

    add register form

--- server/client/src/components/Register.js
+++ server/client/src/components/Register.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Register = () => {
     return(
-        <div>This is a Register placeholder.</div>
+        <div>
+            <form action="/api/login" method="post">
+    <div>
+        <label>Username:</label>
+        <input type="text" name="username"/>
+    </div>
+    <div>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+    </div>
+    <div>
+        <label>Confirm Password:</label>
+        <input type="password" name="confirm-password"/>
+    </div>
+    <div>
+        <input type="submit" value="Register"/>
+    </div>
+</form>
+        </div>
     )
 }
 
