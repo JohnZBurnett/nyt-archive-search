6d2833403aaa26217d8b266fea955ae2c9a91cf0
JohnZBurnett <tuninator@gmail.com>
2018-08-14 09:26:38 -0400

    add user ID to ArticleCollection model

--- server/models/ArticleCollection.js
+++ server/models/ArticleCollection.js
@@ -6,7 +6,8 @@ const articleCollectionSchema = new Schema({
     name: String,
     articles: [ {
         type: mongoose.Schema.Types.ObjectId, ref: 'articles'
-    }]
+    }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'users'}
 });
 
 const ArticleCollection = mongoose.model('articlecollection', articleCollectionSchema); 
