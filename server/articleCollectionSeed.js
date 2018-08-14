a5cdaecc2a3664556813e2eabecef5847caf3eef
JohnZBurnett <tuninator@gmail.com>
2018-08-14 10:15:36 -0400

    confirm via articleCollectionSeed.js testing that users can be populated from collections

--- server/articleCollectionSeed.js
+++ server/articleCollectionSeed.js
@@ -1,24 +1,27 @@
 const ArticleCollection = require('./models/ArticleCollection').ArticleCollection; 
-const Article = require('./models/Article').Article; 
+const Article = require('./models/Article').Article;
+const User = require('./models/User').User; 
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 mongoose.connect(keys.MONGO_DEV_URI); 
 
 (async function findArticle() {
     // const thisArticle = await Article.findById('5b6b46a5f013e485332a7108');
-
+    // const thisUser = await User.findById('5b71f2f1000b754c78646d35'); 
     // const newCollection = new ArticleCollection({
     //     name: 'foo',
-    //     articles: []
+    //     articles: [],
+    //     user: ''
     // })
-    // newCollection.articles.push(thisArticle._id); 
+    // newCollection.articles.push(thisArticle._id);
+    // newCollection.user = thisUser._id; 
 
     // console.log("NEW COLLECTION: ", newCollection);
     
     // newCollection.save(); 
 
-    ArticleCollection.findOne({ name: 'foobar' })
-      .populate('articles')
+    ArticleCollection.findOne({ name: 'foo' })
+      .populate('user')
       .exec( function (err, articleCollection) {
           if (err) return handleError(err);
           console.log(articleCollection); 
