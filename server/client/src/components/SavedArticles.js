0f99def7cd81d645a593fee9fa8a31825a7ae792
JohnZBurnett <tuninator@gmail.com>
2018-08-14 10:21:17 -0400

    write functions in SavedArticle.js to iterate over article collections

--- server/client/src/components/SavedArticles.js
+++ server/client/src/components/SavedArticles.js
@@ -1,17 +1,36 @@
 import React from 'react'; 
 import { connect } from 'react-redux'; 
+import ArticleCard from './ArticleCard'; 
 
 function mapStateToProps(state)  {
     return(
         {
-            articleCollections: state.articleCollections
+            articleCollections: state.articleCollections,
+            articles: state.articles
         }
     )
 }
 
+function renderAllArticleCollectionsForThisUser(articleCollections) {
+    return(
+        articleCollections.map( (articleCollection) => {
+            return (
+             <div>
+                 <h1>Name: {articleCollection.name} </h1>
+                {renderAllArticlesForThisCollection(articleCollection.articles)}
+             </div>
+            )
+        })
+    )
+}
+
+function renderAllArticlesForThisCollection(articleIdArr) {
+    console.log("ARTICLE ID ARR: ", articleIdArr); 
+}
+
 
 const SavedArticles = (props) => {
-    
+    renderAllArticleCollectionsForThisUser(props.articleCollections); 
     return(
         <div>I am a SavedArticles placeholder.</div>
     )
