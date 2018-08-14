436712c714ccc5940e86b4b0453f5c5f46193d62
JohnZBurnett <tuninator@gmail.com>
2018-08-14 10:44:12 -0400

    test to determine why finding article array based on Id does not work in SavedArticles.js

--- server/client/src/components/SavedArticles.js
+++ server/client/src/components/SavedArticles.js
@@ -6,31 +6,41 @@ function mapStateToProps(state)  {
     return(
         {
             articleCollections: state.articleCollections,
-            articles: state.articles
+            articles: state.articleList
         }
     )
 }
 
-function renderAllArticleCollectionsForThisUser(articleCollections) {
+function renderAllArticleCollectionsForThisUser(articleCollections, articles) {
+    console.log("ARTICLE PROPS: ", articles);
     return(
         articleCollections.map( (articleCollection) => {
             return (
              <div>
                  <h1>Name: {articleCollection.name} </h1>
-                {renderAllArticlesForThisCollection(articleCollection.articles)}
+                {renderAllArticlesForThisCollection(articleCollection.articles, articles)}
              </div>
             )
         })
     )
 }
 
-function renderAllArticlesForThisCollection(articleIdArr) {
+function renderAllArticlesForThisCollection(articleIdArr, articleArr) {
+ 
+    return(
+        articleIdArr.map( articleId => {
+            console.log("ARTICLE ID: ", articleId); 
+            const thisArticle = articleArr.find( article => article._id === '5b6b46a5f013e485332a7108')
+            console.log("THIS ARTICLE: ", thisArticle); 
+        })
+    )
     console.log("ARTICLE ID ARR: ", articleIdArr); 
 }
 
 
 const SavedArticles = (props) => {
-    renderAllArticleCollectionsForThisUser(props.articleCollections); 
+
+    renderAllArticleCollectionsForThisUser(props.articleCollections, props.articles); 
     return(
         <div>I am a SavedArticles placeholder.</div>
     )
