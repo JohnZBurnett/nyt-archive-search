c7e68f0526b2823b55ce90b7099e9fcaa428340f
JohnZBurnett <tuninator@gmail.com>
2018-08-19 19:12:17 -0400

    confirm comments are saving to backend

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -78,5 +78,5 @@ export const getArticleCommentsFromApi = async (dispatch, getState) => {
     console.log("GET ARTICLE COMMENTS IS RUNNING"); 
     const articleCommentResults = await axios.get('http://localhost:5000/api/articlecomments'); 
     console.log("ARTICLE COMMENT RESULTS: ", articleCommentResults); 
-    dispatch(updateCurrentArticleComments(articleCommentResults.data)); 
+    dispatch(updateArticleComments(articleCommentResults.data)); 
 }
