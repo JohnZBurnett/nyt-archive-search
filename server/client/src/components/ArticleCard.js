4bfae106454b129041b77f612ed9272d88432301
JohnZBurnett <tuninator@gmail.com>
2018-09-01 19:11:58 -0400

    update headline on card to use only first sentence

--- server/client/src/components/ArticleCard.js
+++ server/client/src/components/ArticleCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArticleCard = (props) => {
     return(<div className="card" onClick={() => props.onClick(props.article, props.history)}>
-        <h5 className="headline-font">{props.article.headline.main}</h5>
+         <h5 className="headline-font">{props.article.headline.main.split(";").length > 1 ? props.article.headline.main.split(';')[0] : props.article.headline.main}</h5>
         <p className="typewriter-font">{props.article.snippet}</p>
     </div>);
 }
